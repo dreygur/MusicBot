9ab57ff2ea2abe1a01f6a695f1da3c87eb94d425
Rakibul Yeasin <ryeasin03@gmail.com>
2021-10-19 01:59:39 +0600

    Added new Feature

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import os
 from discord.ext import commands
 from discord.utils import get
@@ -11,18 +10,27 @@ from youtube_dl import YoutubeDL
 load_dotenv()
 
 # Discord Client
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix=",")
 
 # Login Event
 @client.event
 async def on_ready():
-  print("Bot Logged In...")
+  print(f"Logged in as {client.user} (ID: {client.user.id})")
 
 @client.command()
 async def play(ctx, url):
   YDL_OPTIONS = {
-    "format": "bestaudio",
-    "noplaylist": True
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
   }
   FFMPEG_OPTIONS = {
     "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
@@ -43,9 +51,9 @@ async def play(ctx, url):
     URL = info.get('url')
     voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
     voice.is_playing()
-    await ctx.send("Music is being Played!!!")
+    await ctx.send(f":fire: Playing {info['title']}")
   else:
-    await ctx.send("Music is already Playing...")
+    await ctx.send(":fire: Music is already Playing...")
     return
 
 # Stop Playing Music
@@ -55,7 +63,8 @@ async def stop(ctx):
 
   if voice.is_playing():
     voice.stop()
-    await ctx.send("Music Stopping...")
+    await ctx.voice_client.disconnect()
+    await ctx.send(":fire: Music Stopped...")
 
 # Pause the Music
 @client.command()
@@ -63,7 +72,7 @@ async def pause(ctx):
   voice = get(client.voice_clients, guild=ctx.guild)
   if voice.is_playing():
     voice.pause()
-    await ctx.send("Music Paused...")
+    await ctx.send(":fire: Music Paused...")
 
 # Resume the Music
 @client.command()
@@ -71,7 +80,28 @@ async def resume(ctx):
   voice = get(client.voice_clients, guild=ctx.guild)
   if not voice.is_playing():
     voice.resume()
-    await ctx.send("Music Resumed...")
+    await ctx.send(":fire: Music Resumed...")
+
+# Volume Controll
+@commands.command()
+async def volume(self, ctx, volume: int):
+  if ctx.voice_client is None:
+    return await ctx.send("Not connected to a voice channel.")
+
+  ctx.voice_client.source.volume = volume / 100
+  await ctx.send(f":fire: Changed volume to {volume}%")
+
+# Checks users connectivity
+@play.before_invoke
+async def ensure_voice(self, ctx):
+  if ctx.voice_client is None:
+    if ctx.author.voice:
+      await ctx.author.voice.channel.connect()
+    else:
+      await ctx.send("You are not connected to a voice channel.")
+      raise commands.CommandError("Author not connected to a voice channel.")
+  elif ctx.voice_client.is_playing():
+    ctx.voice_client.stop()
 
 # Run the Bot
 client.run(os.getenv("DISCORD_TOKEN"))
